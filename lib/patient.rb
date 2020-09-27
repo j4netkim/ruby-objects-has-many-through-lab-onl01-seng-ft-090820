c2059e389cfa1cd40d4e01ad844530f5f2784e41
J K <kj688895@gmail.com>
2020-09-27 20:36:06 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -21,4 +21,12 @@ class Patient
     Appointment.new(date, self, doctor)
   end
 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  
+
 end

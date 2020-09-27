7d467974953f325c3676b765f6d271112447a2cb
J K <kj688895@gmail.com>
2020-09-27 20:15:44 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,5 +2,20 @@ require 'pry'
 class Doctor
   attr_accessor :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
+
 
 end

f43f74ec2d3e062ae2328440f0775af1141aaaee
J K <kj688895@gmail.com>
2020-09-27 19:59:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,19 @@ class Song
 
   @@all = []
 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
+
 end

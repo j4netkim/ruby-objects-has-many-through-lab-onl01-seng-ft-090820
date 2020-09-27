fe572c2370afea601cc879032f46e2ed695660b0
J K <kj688895@gmail.com>
2020-09-27 20:22:10 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,20 @@
 require 'pry'
 class Patient
+  attr_accessor :name 
+
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    save 
+  end 
+
+  def save 
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all 
+  end 
 
 end 

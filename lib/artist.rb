f2263a2a59f4b0d4844835d053fc4e8d483e5563
J K <kj688895@gmail.com>
2020-09-27 19:48:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
 
   @@all = []
 
-  def initialize(name)
+  def initialize
     @name = name
     @@all << self
   end
@@ -12,6 +12,8 @@ class Artist
   def self.all
     @@all
   end
-  
+
+
+
 
 end

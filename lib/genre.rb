a872b93fd7e924f88f080196115d9c983457cff9
J K <kj688895@gmail.com>
2020-09-27 20:00:45 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,4 +1,22 @@
 require 'pry'
 class Genre
+  attr_accessor :name
 
-end 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+
+
+end
